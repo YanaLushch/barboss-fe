4b28b214b518c56de465b35c72b6d88ffee57b73
Vitaliy Shebela <vitaliyshebela@Vitaliys-MacBook-Pro.local>
2020-09-20 14:40:05 +0300

    Updated MainPage

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,20 +1,7 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 
+import { Link, Navbar, NavTitle, Page, Toolbar } from "framework7-react";
 import { inject, observer } from "mobx-react";
-
-import {
-  Page,
-  Navbar,
-  Toolbar,
-  Link,
-  NavLeft,
-  NavTitle,
-  NavRight,
-  Popup,
-  Block,
-  Button,
-  f7ready,
-} from "framework7-react";
 
 import { CommonStore } from "../../core/common/CommonStore";
 import { useScrollToTop } from "../../hooks/useScrollToTop";
@@ -23,46 +10,18 @@ type InjectedProps = {
   commonStore: CommonStore;
 };
 
-export const MainPage: FC<RouteParams & InjectedProps> = ({ f7router }) => {
+export const MainPage: FC<RouteParams & InjectedProps> = ({ f7route }) => {
   useScrollToTop();
-
-  useEffect(() => {
-    f7ready((f7) => {
-      console.log("f7 ", f7);
-    });
-  }, [f7router]);
 
   return (
     <Page>
       <Navbar>
-        <NavLeft>
-          <Link>Left Link</Link>
-        </NavLeft>
-        <NavTitle>My App</NavTitle>
-        <NavRight>
-          <Link>Right Link</Link>
-        </NavRight>
+        <NavTitle>Framework7 template</NavTitle>
       </Navbar>
       <Toolbar bottom={true}>
         <Link>Link 1</Link>
-        <Button fill={true} popupOpen=".demo-popup-push">
-          Popup Push
-        </Button>
+        <Link>Link 2</Link>
       </Toolbar>
-      <p>Page content goes here</p>
-      <Link href="/about/">About App</Link>
-      <Popup className="demo-popup-push" push={true}>
-        <Page>
-          <Navbar title="Popup Push">
-            <NavRight>
-              <Link popupClose={true}>Close</Link>
-            </NavRight>
-          </Navbar>
-          <Block strong={true}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-          </Block>
-        </Page>
-      </Popup>
     </Page>
   );
 };
